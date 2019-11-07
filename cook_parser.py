0762074a4513ea3b68eb4e8f7cea7cd713b604ee
Kirill <kkirilenkov@gmail.com>
2019-11-08 02:56:30 +0700

    time check

--- cook_parser.py
+++ cook_parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import os
+from time import time
 PATH = 'C:/Users/Kirill/Desktop/Foods/'
 
 pars_headers = {'accept': '*/*',
@@ -9,6 +10,16 @@ pars_headers = {'accept': '*/*',
                          ' Chrome/77.0.3865.120 Safari/537.36'}
 
 
+def time_it_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        print(time() - start)
+        return result
+    return wrapper
+
+
+@time_it_decorator
 def food_parser(url, headers):
     titles = []
     sess = requests.Session()
@@ -16,7 +27,7 @@ def food_parser(url, headers):
     if request.status_code == 200:
         print('LOADED')
         soup = bs(request.content, 'html.parser')
-        items = soup.find_all('h3', {'class': lambda x: x and 'horizontal-tile__item-title' in x.split()})
+        items = soup.find_all('h3', class_='horizontal-tile__item-title')
         for item in items:
             for tag in item.find_all('span'):
                 recipe_title = tag.text.strip()
@@ -26,10 +37,11 @@ def food_parser(url, headers):
     return titles
 
 
+@time_it_decorator
 def main():
     os.chdir(PATH)
     common_titles = []
-    for i in range(93):
+    for i in range(10):
         print(f'Загружается страница № {i + 1}')
         pars_url = f'https://eda.ru/recepty/osnovnye-blyuda/russkaya-kuhnya?page={i + 1}'
         common_titles.append(food_parser(pars_url, pars_headers))
